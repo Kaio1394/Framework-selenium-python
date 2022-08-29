8be527b7eb5d6bd54c89ce5f06981cd453becec5
Kaio Cândido Santiago <kaio.santiago.13@hotmail.com>
2022-08-28 21:27:59 -0300

    Adicionado novas aplicações no helper

--- pages/Helper.py
+++ pages/Helper.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from driver.DriverUtil import DriverUtil
 
 
@@ -11,14 +12,38 @@ class Helper:
     def get_url(self, value):
         self.driver.get(value)
 
-    def send_keys(self, by, by_value, value):
-        self.driver.find_element(by, by_value).send_keys(value)
+    def send_keys(self, options_by, by_value, value):
+        match str(options_by).lower():
+            case 'name':
+                self.driver.find_element(By.NAME, by_value).send_keys(value)
+            case 'xpath':
+                self.driver.find_element(By.XPATH, by_value).send_keys(value)
+            case 'id':
+                self.driver.find_element(By.ID, by_value).send_keys(value)
 
-    def send_click(self, by, by_value):
-        self.driver.find_element(by, by_value).click()
+    def send_click(self, options_by, by_value):
+        match str(options_by).lower():
+            case 'name':
+                self.driver.find_element(By.NAME, by_value).click()
+            case 'xpath':
+                self.driver.find_element(By.XPATH, by_value).click()
+            case 'id':
+                self.driver.find_element(By.ID, by_value).click()
 
     def close(self):
         self.driver.close()
 
     def get_url_browser(self):
         return self.driver.current_url
+
+    def wait_until(self, timeout, options_by, value):
+        match str(options_by).lower():
+            case 'name':
+                WebDriverWait(self.driver, timeout)\
+                    .until(EC.presence_of_element_located((By.NAME, value)))
+            case 'xpath':
+                WebDriverWait(self.driver, timeout) \
+                    .until(EC.presence_of_element_located((By.XPATH, value)))
+            case 'id':
+                WebDriverWait(self.driver, timeout) \
+                    .until(EC.presence_of_element_located((By.ID, value)))
